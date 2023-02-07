d8ef05c18e791e6021293f0df44e48f872ad7886
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-02-07 10:04:45 +0700

    app react native

--- client/App.js
+++ client/App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ProductList } from "./screens/ProductList";
+import { ProductDetail } from "./screens/ProductDetail";
+import { InvoiceProvider } from "./InvoiceContext";
+import { InvoiceIcon } from "./components/InvoiceIcon";
+import { Invoice } from "./screens/Invoice";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <InvoiceProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Products"
+            component={ProductList}
+            options={({ navigation }) => ({
+              title: "Products",
+              headerRight: () => <InvoiceIcon navigation={navigation} />,
+            })}
+          />
+          <Stack.Screen
+            name="ProductDetail"
+            component={ProductDetail}
+            options={({ navigation }) => ({
+              title: "Products",
+              headerRight: () => <InvoiceIcon navigation={navigation} />,
+            })}
+          />
+          <Stack.Screen
+            name="Invoice"
+            component={Invoice}
+            options={({ navigation }) => ({
+              title: "Products",
+              headerRight: () => <InvoiceIcon navigation={navigation} />,
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </InvoiceProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  Container: {
+    textAlign: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
